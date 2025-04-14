47599e00226b1c95606b7c828c23d609597e3576
Brandsteps-star <webdevbrandsteps6@gmail.com>
2025-04-14 17:26:42 +0500

    Home Page completed

--- src/components/PreFooter.jsx
+++ src/components/PreFooter.jsx
@@ -3,7 +3,7 @@ import ShuffleText from "../effects/ShuffleText"
 export const PreFooter = () => {
     return(
         <section className="prefooter min-h-[50vh] h-full w-full relative z-20 flex justify-center font-whyte-inktrap bg-black text-white">
-                <div className="w-full max-md:w-full flex z-30 relative max-md:flex-col overflow-hidden border-b border-[ffffff2a]">
+                <div className="w-full max-md:w-full flex z-30 relative max-md:flex-col overflow-hidden border-b border-[#ffffff2a]">
                     <div className="w-1/4 h-full border-r border-[#ffffff2a] max-sm:border-r-0 max-sm:border-b p-8 flex items-end max-sm:w-full">
                         <ShuffleText
                             text={
@@ -13,7 +13,7 @@ export const PreFooter = () => {
                             speed={1}
                         />
                     </div>
-                    <div className="w-1/4 h-full border-r border-[#ffffff2a] max-sm:border-r-0 max-sm:border-b p-8 flex flex-col items-start justify-between max-sm:w-full">
+                    <div className="w-1/4 h-full border-r border-[#ffffff2a] max-sm:border-r-0 max-sm:border-b p-8 flex flex-col items-start justify-between max-sm:w-full max-sm:gap-6">
                         <ShuffleText
                                 text={"Discover More"}
                                 className="inline-block w-full whitespace-pre-line break-words font-mono text-[9px] uppercase"
@@ -30,7 +30,7 @@ export const PreFooter = () => {
                             }
                         </ul>
                     </div>
-                    <div className="w-1/4 h-full border-r border-[#ffffff2a] max-sm:border-r-0 max-sm:border-b p-8 flex flex-col items-start justify-between max-sm:w-full">
+                    <div className="w-1/4 h-full border-r border-[#ffffff2a] max-sm:border-r-0 max-sm:border-b p-8 flex flex-col items-start justify-between max-sm:w-full max-sm:gap-6">
                         <ShuffleText
                                     text={"JOIN THE CONVERSATION"}
                                     className="inline-block w-full whitespace-pre-line break-words font-mono text-[9px] uppercase"
